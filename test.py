eb49ce3e7911d664f0c7727c5a4decdd7072511b
tavkh <tavkhelidzeluka@gmail.com>
2022-03-21 20:16:11 +0400

    Add guard clause && function for printing elements

---
+++ test.py
@@ -0,0 +1,18 @@
+"""
+This module is designed for test use only!
+"""
+
+a = 5
+
+def print_list_items(gela: list[any]):
+    for item in gela:
+        print(item)
+
+
+if __name__ == '__main__':
+    print(__name__)
+
+    print(__doc__)
+
+    print(5 + 6)
+
